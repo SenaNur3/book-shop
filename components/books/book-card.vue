2b2be8543cc0fb94ece9eebd785c4b313fc193a8
SenaNur3 <se_na_e12@hotmail.com>
2024-04-29 19:50:12 +0300

    book card

---
+++ components/books/book-card.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="w-[200px] flex flex-col border rounded-lg h-[200px] justify-between p-[16px]">
+        <img :src="book?.imageUrl" alt="Book Cover" />
+        <div class="book-details">
+            <h3>{{ book?.title }}</h3>
+            <p>Author: {{ book?.author }}</p>
+            <p>Price: ${{ book?.price }}</p>
+            <p>Pages: {{ book?.pages }}</p>
+        </div>
+    </div>
+</template>
+
+<script setup props="book">
+
+</script>
--- components/books/book-main.vue
+++ components/books/book-main.vue
@@ -1,10 +1,52 @@
 <template>
-    <div>
-        book main
-    </div>
+  <div class="flex justify-center gap-2 mt-[20px]">
+    <BookCard v-for="book in books" :key="book.id" :book="book" />
+  </div>
 </template>
 
-<script>
+<script setup>
+import BookCard from './book-card.vue';
 
-
+ const books = [
+    {
+      id: 1,
+      title: 'To Kill a Mockingbird',
+      author: 'Harper Lee',
+      price: 10.99,
+      pages: 336,
+      imageUrl: 'https://via.placeholder.com/150',
+    },
+    {
+      id: 2,
+      title: '1984',
+      author: 'George Orwell',
+      price: 9.99,
+      pages: 328,
+      imageUrl: 'https://via.placeholder.com/150',
+    },
+    {
+      id: 3,
+      title: 'The Great Gatsby',
+      author: 'F. Scott Fitzgerald',
+      price: 12.50,
+      pages: 180,
+      imageUrl: 'https://via.placeholder.com/150',
+    },
+    {
+      id: 4,
+      title: 'Brave New World',
+      author: 'Aldous Huxley',
+      price: 11.25,
+      pages: 288,
+      imageUrl: 'https://via.placeholder.com/150',
+    },
+    {
+      id: 5,
+      title: 'Pride and Prejudice',
+      author: 'Jane Austen',
+      price: 8.75,
+      pages: 432,
+      imageUrl: 'https://via.placeholder.com/150',
+    },
+  ];
 </script>
